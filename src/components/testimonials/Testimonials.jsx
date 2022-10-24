e97107ce51cc3a2e402697108459734e2eaa8e2b
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-24 23:06:53 +0000

    getting started with the testimonial page

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./testimonial.css";
 
 const Testimonials = () => {
-  return <section id="testimonial">Testimonials</section>;
+  return (
+    <section id="testimonial">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonials"></article>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
