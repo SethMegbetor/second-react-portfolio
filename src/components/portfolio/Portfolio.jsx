26c08bf1ceb3ff5b6565c8939d0048342801719f
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-24 23:03:25 +0000

    done with the article tag and data now being mapped dynamically

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -65,18 +65,34 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        {
-          
-          data.map(({ id, image, title, github, demo }) => {
-            return (
-              <article className="portfolio__item">
-                <div className="porfolio__item-image">
-                  <img src={IMG1} alt="" />
-                </div>
-              </article>
-            )
-          })
-        }
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="porfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="porfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
