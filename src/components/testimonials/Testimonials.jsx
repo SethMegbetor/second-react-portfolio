a205eefdefdf409a717e2066ca39f87e62957de9
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-25 10:43:32 +0000

    added some content to the testimonial.jsx file

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./testimonial.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
 
 const Testimonials = () => {
   return (
@@ -8,7 +12,50 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonials"></article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Jane Doe</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              debitis error molestias sequi dolor tempora dicta quibusdam
+              repudiandae labore fugiat.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Jane Doe</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              debitis error molestias sequi dolor tempora dicta quibusdam
+              repudiandae labore fugiat.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Jane Doe</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              debitis error molestias sequi dolor tempora dicta quibusdam
+              repudiandae labore fugiat.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className="client__name">Jane Doe</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              debitis error molestias sequi dolor tempora dicta quibusdam
+              repudiandae labore fugiat.
+            </small>
+          </div>
+        </article>
       </div>
     </section>
   );
