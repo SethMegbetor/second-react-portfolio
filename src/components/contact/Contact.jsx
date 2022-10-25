42f78089f99d70145791ba9af2a9c6d6c60e1d91
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-25 15:33:48 +0000

    done with the contact component, can receive email from email js

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+// import React from "react";
+
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsTwitter } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        // "YOUR_SERVICE_ID"
+        // "YOUR_TEMPLATE_ID",
+        // "YOUR_PUBLIC_KEY"
+        "service_t75ewwm",
+        "template_aiupo1d",
+        form.current,
+        "sOrNMCZr1ZxJMh0OL"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -12,7 +42,7 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail className="contact__option-icon"/>
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>sethmegbetor@gmail.com</h5>
             <a
@@ -24,7 +54,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <BsTwitter className="contact__option-icon"/>
+            <BsTwitter className="contact__option-icon" />
             <h4>Twitter</h4>
             <h5>@realCodeMVP</h5>
             <a
@@ -36,7 +66,7 @@ const Contact = () => {
             </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp className="contact__option-icon"/>
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+233 247 577 679</h5>
             <a
@@ -48,11 +78,11 @@ const Contact = () => {
             </a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your full name" />
           <input type="text" name="email" placeholder="Your email" />
           <textarea
-            name="messge"
+            name="message"
             rows="7"
             placeholder="Your Message"
           ></textarea>
